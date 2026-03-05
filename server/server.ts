6bd78f7fdee38abca8bac2bc4d22050441688465
Mina R <minarumang@icloud.com>
2026-03-05 17:04:20 +0100

    starting code

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,81 @@
 import { z } from "zod";
 import express from "express";
 
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
 //define a schema for a username
-const usernameschema = z.string().min(3).max(10);
+const user = { name: "Mina123", age: 24 };
 
-//test valid data
-const validatedUsername = usernameschema.safeParse("Mina123");
-console.log(validatedUsername);
+const userschema = z.object({
+  name: z.string().min(3).max(10),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old." })
+    .max(100, { message: "you must be at most be 100 years old" })
+    .optional()
+    .default(28),
+  email: z.email(),
+});
 
-//test invalid data
-try {
-  const validatedUsername = usernameschema.safeParse("Mi");
-  console.log(validatedUsername);
-} catch (error) {
-  console.error(error);
+const randomUserResponseSchema = z.object({
+  results: z.array(
+    z.object({
+      name: z.object({
+        first: z.string(),
+        last: z.string(),
+      }),
+      country: z.string(),
+    }),
+  ),
+});
+
+app.get("/random-user", async (req, res) => {
+  try {
+    const response = await fetch("https://randomuser.me/api/");
+    const data = await response.json();
+
+    const validatedRandomUser = randomUserResponseSchema.safeParse(data);
+    if (!validatedRandomUser.success) {
+      return res.status(500).json({
+        error: "Invalid data from RandomUser API",
+        details: validatedRandomUser.error,
+      });
+    }
+    const randomUser = validatedRandomUser.data.results[0];
+    res.json({
+      name: `${randomUser?.name.first} ${randomUser?.name.last}`,
+      email: randomUser?.country,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "Failed to fetch random user",
+    });
+  }
+});
+
+const validatedUsername = userschema.safeParse(user);
+
+if (!validatedUsername.success) {
+  console.error(validatedUsername.error);
+} else {
+  console.log(validatedUsername.data);
 }
+
+app.post("/users", (req, res) => {
+  const validatedNewUser = userschema.safeParse(req.body);
+  if (!validatedNewUser.success) {
+    return res.status(400).json({
+      error: "Invalid user data",
+      details: validatedNewUser.error,
+    });
+    //console.error(validatedNewUser.error);
+  } else {
+    res.status(201).json({ user: validatedNewUser });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
