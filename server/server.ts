7fc2674fc3fdc83dc85dfb738f5b4ee106f47a2e
Mina R <minarumang@icloud.com>
2026-03-05 15:20:47 +0100

    fix resolve nodemon/ts-node dev startup issue

--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,17 @@
 import { z } from "zod";
 import express from "express";
+
+//define a schema for a username
+const usernameschema = z.string().min(3).max(10);
+
+//test valid data
+const validatedUsername = usernameschema.safeParse("Mina123");
+console.log(validatedUsername);
+
+//test invalid data
+try {
+  const validatedUsername = usernameschema.safeParse("Mi");
+  console.log(validatedUsername);
+} catch (error) {
+  console.error(error);
+}
